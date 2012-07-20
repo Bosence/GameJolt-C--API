77886e10413bea5dfe81f369e6604271f5a62fb9
HyperGod <pyromaniacchris@gmail.com>
2012-07-20 12:21:36 +0100

    Further strict conventions, keypair work
    Time for lunch. :P

--- GameJoltAPI/Helpers/Keypair.cs
+++ GameJoltAPI/Helpers/Keypair.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Net;
 
 namespace GameJoltAPI.Helpers
 {
@@ -13,6 +15,35 @@ namespace GameJoltAPI.Helpers
     /// </summary>
     public static class Keypair
     {
+        public static Dictionary<string, string> getData(string completeUrl)
+        {
+            Uri u = new Uri(completeUrl);
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(u);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            Stream st = res.GetResponseStream();
 
+            Dictionary<string, string> temp = new Dictionary<string, string>();
+            
+            /* Read the stream line by line, to parse the gamejoltapi format
+             * TODO: add success/failure checks, parsing checks
+             */
+            using (StreamReader sr = new StreamReader(st))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    try
+                    {
+                        string line = sr.ReadLine();
+                        temp.Add(line.Split(':')[0], line.Split(':')[1]);
+                    }
+                    catch (Exception e)
+                    {
+                        // do something
+                    }
+                }
+            }
+
+            return temp;
+        }
     }
 }
